21ee7ae2dbfacf8ce758da4c6d59f442ffcb2d93
eonju <eonju.dev@gmail.com>
2021-02-05 12:11:50 +0900

    feat : Car 객체 메소드 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,5 +3,16 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter car name : ");
+        String carNames = scanner.next();
+        System.out.print("Enter round : ");
+        int round = scanner.nextInt();
+
+        String[] names = carNames.split(",");
+        Car[] cars = new Car[names.length];
+        for (int i = 0; i < names.length; i++){
+            cars[i] = new Car(names[i]);
+        }
     }
 }
--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -5,4 +5,12 @@ public class Car {
     public Car(String name) {
         this.name = name;
     }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPosition(){
+        return position;
+    }
 }
